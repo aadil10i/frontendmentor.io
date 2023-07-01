5e08911fa90ddd6c8db14e686d064edade02741d
aadil10i <aadil3003@outlook.com>
2023-07-01 22:21:53 +0400

    summary component card

--- results-summary-component/tailwind.config.ts
+++ results-summary-component/tailwind.config.ts
@@ -3,7 +3,17 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pale-blue": "hsl(221, 100%, 96%)",
+        "violet-blue": "hsla(256, 72%, 46%, 1)",
+        "persian-blue": "hsla(241, 72%, 46%, 1)",
+        "light-slate-blue": "hsl(252, 100%, 67%)",
+        "light-royal-blue": "hsl(241, 81%, 54%)",
+        "dark-gray-blue": "hsl(224, 30%, 27%)",
+        "light-lavander": "hsl(241, 100%, 89%)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
